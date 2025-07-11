fd22257e4477f94c48fb04efe47299576dca7884
tanmay0996 <167513949+tanmay0996@users.noreply.github.com>
2025-07-11 20:08:35 +0530

    user Registration

---
+++ app/api/auth/register/route.ts
@@ -0,0 +1,53 @@
+import { NextRequest, NextResponse } from "next/server";
+import User from "@/models/User";
+import { connectToDatabase } from "@/lib/db";
+
+export async function POST(request: NextRequest) {
+  try {
+    const { email, password } = await request.json();
+
+    if (!email || !password) {
+      return NextResponse.json(
+        { error: "email and password are required" },
+        { status: 400 }
+      );
+    }
+
+    await connectToDatabase();
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return NextResponse.json(
+        {
+          error: "user already exist",
+        },
+        {
+          status: 500,
+        }
+      );
+    }
+
+
+    await User.create(
+        {
+            email,
+            password
+        }
+    )
+
+   return NextResponse.json(
+    {message:"User is registered successfully"},
+    {status:200}
+   )
+
+  } catch (error) {
+
+    console.error("Registration error",error)
+    return NextResponse.json(
+        {
+            error:"Registration failed"
+        },
+        {status:500}
+    )
+  }
+}
